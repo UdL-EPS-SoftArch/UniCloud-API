3659c5e565beef1344bd7a7726fcab5043445987
adriafabra <adria.fabra2508@gmail.com>
2022-02-22 16:21:02 +0100

    Defining University Repository

--- src/main/java/cat/udl/eps/softarch/unicloud/repository/UniversityRepository.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/repository/UniversityRepository.java
@@ -2,14 +2,11 @@ package cat.udl.eps.softarch.unicloud.repository;
 
 import cat.udl.eps.softarch.unicloud.domain.University;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UniversityRepository extends PagingAndSortingRepository<University, Long> {
-    List<University> findByName(String name);
+    List<University> findByName( String name);
     List<University> findByCountry(String country);
     List<University> findByCity(String city);
-    List<University> findByAcronym(String acronym);
-    List<University> findByNameContaining(String name);
 }
