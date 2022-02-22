88981a293c2a28bb3528db610fdffe4e08293f72
gabygarcia98 <gabygarcia211998@gmail.com>
2022-02-22 16:45:04 +0100

    Added degree repository.

--- src/main/java/cat/udl/eps/softarch/unicloud/repository/DegreeRepository.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/repository/DegreeRepository.java
@@ -10,11 +10,7 @@ import java.util.List;
 @RepositoryRestResource
 public interface DegreeRepository extends PagingAndSortingRepository<Degree, Long> {
 
-    List<Degree> findByFaculty(String nameFaculty);
+    List<Degree> findByFacultyContaining(@Param("nameFaculty") String nameFaculty);
 
-    List<Degree> findByFacultyContaining(String nameFaculty);
-
-    List<Degree> findByName(String nameDegree);
-
-    List<Degree> findByNameContaining(String nameDegree);
+    List<Degree> findByNameContaining(@Param("nameDegree") String nameDegree);
 }
