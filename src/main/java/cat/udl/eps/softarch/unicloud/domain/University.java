3175388e971f75db8a99a524fc3575860e05e354
adriafabra <adria.fabra2508@gmail.com>
2022-02-22 15:59:16 +0100

    University model and constraints

---
+++ src/main/java/cat/udl/eps/softarch/unicloud/domain/University.java
@@ -0,0 +1,35 @@
+package cat.udl.eps.softarch.unicloud.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class University extends UriEntity<Long> {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String country;
+    @NotBlank
+    private String city;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
