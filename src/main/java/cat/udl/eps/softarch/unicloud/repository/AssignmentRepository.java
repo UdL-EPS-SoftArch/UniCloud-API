7fe167f25388347e8cfe8b0dc3df57fa5bce2103
GuillemBetriaPerez <guillebetri2000@gmail.com>
2022-02-22 16:58:43 +0100

    Creation of the Assignment repository interface.

---
+++ src/main/java/cat/udl/eps/softarch/unicloud/repository/AssignmentRepository.java
@@ -0,0 +1,7 @@
+package cat.udl.eps.softarch.unicloud.repository;
+
+import cat.udl.eps.softarch.unicloud.domain.Assignment;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AssignmentRepository extends PagingAndSortingRepository<Assignment, Long> {
+}
