f88f75b1d184885bc172251ddd67e96144418645
Buisi97 <oscar.buisan@gmail.com>
2022-02-23 18:17:12 +0100

    Subject Feature model created

--- src/main/java/cat/udl/eps/softarch/unicloud/domain/Subject.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/domain/Subject.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
@@ -23,6 +25,8 @@ public class Subject extends UriEntity<Long>{
     private String name;
 
     @NotBlank
+    @Min(value = 1)
+    @Max(value = 5)
     private BigDecimal course;
 
     @NotBlank
