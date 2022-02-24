3b2b68598c963a44e2c0ba0783e183c164de4676
joanss2 <joanss2@hotmail.com>
2022-02-24 15:34:48 +0100

    added class

--- src/main/java/cat/udl/eps/softarch/unicloud/domain/Rating.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/domain/Rating.java
@@ -1,14 +1,10 @@
 package cat.udl.eps.softarch.unicloud.domain;
 
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
@@ -21,21 +17,11 @@ public class Rating {
         @Id
         private Long id;
 
-        @ManyToOne
-        @JsonIdentityReference(alwaysAsId = true)
-        private Student author;
-
-
-        @ManyToOne
-        private Resource resourceRated;
-
-        @NotNull
-        @Max(10)
-        @Min(0)
+        @NotBlank
+        @Column(unique = true)
         private BigDecimal rating;
 
         @NotBlank
         private String comment;
-
 
 }
