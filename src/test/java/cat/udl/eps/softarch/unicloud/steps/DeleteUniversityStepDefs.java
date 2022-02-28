50ed4c3e547a4c5c6a8a853d1cbfb9f8f3ba7287
ajs <estudisjuveadria@gmail.com>
2022-02-28 18:06:28 +0100

    Added basic Delete University features and steps

--- src/test/java/cat/udl/eps/softarch/unicloud/steps/DeleteUniversityStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/unicloud/steps/DeleteUniversityStepDefs.java
@@ -15,8 +15,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class DeleteUniversityStepDefs {
+    String newResourcesUri;
     final StepDefs stepDefs;
     final UniversityRepository universityRepository;
+    public static String id;
 
     DeleteUniversityStepDefs(StepDefs stepDefs, UniversityRepository universityRepository) {
         this.stepDefs = stepDefs;
@@ -25,21 +27,27 @@ public class DeleteUniversityStepDefs {
 
     @When("I remove a University with name {string}")
     public void removeUniversity(String name) throws Exception {
-        List<University> universities = universityRepository.findByName(name);
-        String id = universities.size() > 0 ? universities.get(0).getId().toString():"666";
-        stepDefs.result = stepDefs.mockMvc.perform(delete("/universities/{id}",id).contentType(MediaType.APPLICATION_JSON)
+        University university = universityRepository.findByName(name).get(0);
+        assert university.getId() != null;
+        stepDefs.result = stepDefs.mockMvc.perform(delete("/universities/"+university.getId().toString()).contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
     }
-
+    @When("I remove a non-existent University")
+    public void removeNonExistent() throws  Exception {
+        stepDefs.result = stepDefs.mockMvc.perform(delete("/universities/666").contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
     @Then("The University with name {string} has not been removed")
     public void notRemovedCheck(String name) throws Exception {
         List<University> universities = universityRepository.findByName(name);
         assert universities.size() == 1;
     }
 
-    @And("The University with name {string} has been removed")
+    @Then("The University with name {string} has been removed")
     public void removedCheck(String name) throws Exception {
         List<University> universities = universityRepository.findByName(name);
         assert universities.size() == 0;
--- src/test/resources/features/DeleteUniversity.feature
+++ src/test/resources/features/DeleteUniversity.feature
@@ -1,8 +1,7 @@
 Feature: DELETE a University
 
   Background:
-    Given There is a registered student with username "student" and password "password" and email "student@demo.com"
-    Given There is a registered admin with username "admin" and password "password" and email "admin@demo.com"
+    Given There is a registered user with username "demo" and password "password" and email "demo@demo.com"
     Given There is a university with name "Universitat de Lleida", acronym "UDL", country "Spain", city "Lleida"
 
   Scenario: Delete a University when not authenticated
@@ -12,19 +11,20 @@ Feature: DELETE a University
     And The University with name "Universitat de Lleida" has not been removed
 
   Scenario: Delete a University when authenticated as a student
-    Given I login as "student" with password "password"
+    Given I login as "demo" with password "password"
     When I remove a University with name "Universitat de Lleida"
-    Then The response code is 403
+    Then The response code is 401
     And The University with name "Universitat de Lleida" has not been removed
 
+    #FALTA FICAR TEST USUARI STUDENT I ROLS
   Scenario: Delete a University when authenticated as an admin
-    Given I login as "admin" with password "password"
+    Given I login as "demo" with password "password"
     When I remove a University with name "Universitat de Lleida"
     Then The response code is 204
     And The University with name "Universistat de Lleida" has been removed
 
     Scenario: Delete a non-existing University when authenticated as an admin
-      Given I login as "admin" with password "password"
-      When I remove a University with name "Universitat de Barcelona"
+      Given I login as "demo" with password "password"
+      When I remove a non-existent University
       Then The response code is 404
 
