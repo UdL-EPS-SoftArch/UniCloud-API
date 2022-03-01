f60f258d13673f73b235860ad4ac4f2e1d3c8bfb
Marc <marc-saba@hotmail.com>
2022-03-01 16:09:23 +0100

    delete rating feature and StepDefs added

---
+++ src/test/java/cat/udl/eps/softarch/unicloud/steps/DeleteRatingStepDefs.java
@@ -0,0 +1,22 @@
+package cat.udl.eps.softarch.unicloud.steps;
+
+import cat.udl.eps.softarch.unicloud.domain.Rating;
+import cat.udl.eps.softarch.unicloud.repository.RatingRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.java.en.When;
+import org.springframework.http.MediaType;
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+public class DeleteRatingStepDefs {
+    final StepDefs stepDefs;
+    final RatingRepository ratingRepository;
+
+    DeleteRatingStepDefs(StepDefs stepDefs, RatingRepository universityRepository) {
+        this.stepDefs = stepDefs;
+        this.ratingRepository = universityRepository;
+    }
+}
---
+++ src/test/resources/features/ModifyRating.feature
@@ -0,0 +1,1 @@
+Feature: DELETE a Rating
