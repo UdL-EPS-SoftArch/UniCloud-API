b2ac7a7c55a1c11e82314bf12eb6a130b5259ade
Armand <56553013+armandciutat@users.noreply.github.com>
2022-03-02 18:41:31 +0100

    Created StepDefs with failed tests. TODO

--- src/test/java/cat/udl/eps/softarch/unicloud/steps/DeleteUserStepDef.java
+++ src/test/java/cat/udl/eps/softarch/unicloud/steps/DeleteUserStepDef.java
@@ -1,25 +1,18 @@
 package cat.udl.eps.softarch.unicloud.steps;
 
-import cat.udl.eps.softarch.unicloud.repository.UserRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-
 public class DeleteUserStepDef {
-    final StepDefs stepDefs;
-    final UserRepository userRepository;
-
-    public DeleteUserStepDef(StepDefs stepDefs, UserRepository userRepository) {
-        this.stepDefs = stepDefs;
-        this.userRepository = userRepository;
+    @When("I delete the user with username {string}")
+    public void iDeleteTheUserWithUsername(String arg0) {
     }
 
-    @When("I delete the user with username {string}")
-    public void iDeleteTheUserWithUsername(String username) throws Exception {
-        stepDefs.result = stepDefs.mockMvc.perform(
-                delete("/users/{username}", username)
-                        .with(AuthenticationStepDefs.authenticate()));
+    @And("It does not exist a user with username {string}")
+    public void itDoesNotExistAUserWithUsername(String arg0) {
+    }
+
+    @And("It has not been deleted a user with username {string}")
+    public void itHasNotBeenDeletedAUserWithUsername(String arg0) {
     }
 }
