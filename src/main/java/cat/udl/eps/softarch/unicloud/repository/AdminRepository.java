f7458e2bfcbf337a650dd84e86988fb3fadc5506
Armand <56553013+armandciutat@users.noreply.github.com>
2022-03-16 18:12:07 +0100

    Created AdminRepository

---
+++ src/main/java/cat/udl/eps/softarch/unicloud/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package cat.udl.eps.softarch.unicloud.repository;
+
+import cat.udl.eps.softarch.unicloud.domain.Admin;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface AdminRepository extends PagingAndSortingRepository<Admin, Long> {
+
+}
