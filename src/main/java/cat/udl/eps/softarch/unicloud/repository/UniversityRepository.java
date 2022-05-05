f01b36c53ec017a5d24a5d09efb2d41c531b84db
ajs <estudisjuveadria@gmail.com>
2022-05-05 16:22:04 +0200

    Added University findByEveryDataType to university repository.

--- src/main/java/cat/udl/eps/softarch/unicloud/repository/UniversityRepository.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/repository/UniversityRepository.java
@@ -1,6 +1,8 @@
 package cat.udl.eps.softarch.unicloud.repository;
 
 import cat.udl.eps.softarch.unicloud.domain.University;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +14,5 @@ public interface UniversityRepository extends PagingAndSortingRepository<Univers
     List<University> findByCity(String city);
     List<University> findByAcronym(String acronym);
     List<University> findByNameContaining(String name);
+    Page<University> findByNameContainingOrAcronymContainingOrCountryContainingOrCityContaining(String name, String acronym, String country, String city, Pageable pageable);
 }
