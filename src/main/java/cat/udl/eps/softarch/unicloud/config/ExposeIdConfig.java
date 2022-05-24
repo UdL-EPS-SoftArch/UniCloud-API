952bb058b940de7f782fa26c6233a6e1d411292d
Buisi97 <oscar.buisan@gmail.com>
2022-05-24 15:45:47 +0200

    Test

--- src/main/java/cat/udl/eps/softarch/unicloud/config/ExposeIdConfig.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/config/ExposeIdConfig.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.unicloud.config;
 import cat.udl.eps.softarch.unicloud.domain.Degree;
 import cat.udl.eps.softarch.unicloud.domain.Rating;
+import cat.udl.eps.softarch.unicloud.domain.Subject;
 import cat.udl.eps.softarch.unicloud.domain.University;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -11,6 +12,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 public class ExposeIdConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry regs) {
-        config.exposeIdsFor(University.class, Degree.class, Rating.class);
+        config.exposeIdsFor(University.class, Degree.class, Rating.class, Subject.class);
     }
 }
