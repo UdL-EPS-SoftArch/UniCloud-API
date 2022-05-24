034176c72f5b6cf6f872dfb320e7ee5fff9ad93b
bergacat1 <albertbergagatius@gmail.com>
2022-05-24 16:54:30 +0200

    Fix BBDDInitialization

--- src/main/java/cat/udl/eps/softarch/unicloud/config/BBDDInitialization.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/config/BBDDInitialization.java
@@ -4,6 +4,7 @@ import cat.udl.eps.softarch.unicloud.domain.*;
 import cat.udl.eps.softarch.unicloud.repository.*;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -23,8 +24,11 @@ public class BBDDInitialization {
     final SubjectRepository subjectRepository;
     final ResourceRepository resourceRepository;
     final RatingRepository ratingRepository;
+    final UserRepository userRepository;
 
 
+    @Value("${default-password}")
+    String defaultPassword;
 
     public BBDDInitialization(AdminRepository adminRepository,
                               StudentRepository studentRepository,
@@ -32,7 +36,8 @@ public class BBDDInitialization {
                               DegreeRepository degreeRepository,
                               SubjectRepository subjectRepository,
                               ResourceRepository resourceRepository,
-                              RatingRepository ratingRepository) {
+                              RatingRepository ratingRepository,
+                              UserRepository userRepository) {
         this.adminRepository = adminRepository;
         this.studentRepository = studentRepository;
         this.universityRepository = universityRepository;
@@ -40,11 +45,38 @@ public class BBDDInitialization {
         this.subjectRepository = subjectRepository;
         this.resourceRepository = resourceRepository;
         this.ratingRepository = ratingRepository;
+        this.userRepository = userRepository;
     }
 
     @PostConstruct
     public void initializeDatabase() {
-
+        // Sample user
+        if (!userRepository.existsById("demo")) {
+            User player = new User();
+            player.setEmail("demo@sample.app");
+            player.setUsername("demo");
+            player.setPassword(defaultPassword);
+            player.encodePassword();
+            userRepository.save(player);
+        }
+        // Sample admin
+        if(!adminRepository.existsById("adminDemo")){
+            Admin admin = new Admin();
+            admin.setEmail("adminDemo@sample.app");
+            admin.setUsername("admin");
+            admin.setPassword(defaultPassword);
+            admin.encodePassword();
+            adminRepository.save(admin);
+        }
+        // Sample student
+        if(!studentRepository.existsById("Deme")){
+            Student student = new Student();
+            student.setEmail("studentDemo@sample.app");
+            student.setUsername("student");
+            student.setPassword(defaultPassword);
+            student.encodePassword();
+            studentRepository.save(student);
+        }
         University university = new University();
         university.setName("Universitat Lleida");
         university.setAcronym("ULL");
@@ -96,7 +128,7 @@ public class BBDDInitialization {
         resource.setName("gei");
         resource.setDescription("Aixo es una prova");
         resource.setFile("file");
-        resource.setOwner(new Student());
+        resource.setOwner(studentRepository.findById("student").get());
         resource.setSubjects(subjects_list);
         resource.setResourceType(Resource.ResourceType.note);
         resourceRepository.save(resource);
