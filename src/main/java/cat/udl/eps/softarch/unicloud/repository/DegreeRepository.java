0ab9daff8a8158f4a786a7f9a7079f66fea64255
Iñaki <serranoinyaki@gmail.com>
2022-05-31 16:25:15 +0200

    Added findByComplete to use in degree-search-component

--- src/main/java/cat/udl/eps/softarch/unicloud/repository/DegreeRepository.java
+++ src/main/java/cat/udl/eps/softarch/unicloud/repository/DegreeRepository.java
@@ -2,6 +2,8 @@ package cat.udl.eps.softarch.unicloud.repository;
 
 import cat.udl.eps.softarch.unicloud.domain.Degree;
 import cat.udl.eps.softarch.unicloud.domain.University;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -11,16 +13,15 @@ import java.util.List;
 public interface DegreeRepository extends PagingAndSortingRepository<Degree, Long> {
 
     List<Degree> findByFaculty(String nameFaculty);
-
     List<Degree> findByFacultyContaining(String nameFaculty);
 
     List<Degree> findByName(String nameDegree);
-
     List<Degree> findByNameContaining(String nameDegree);
 
     List<Degree> findByUniversityName(String nameUni);
-
     List<Degree> findByUniversityNameContaining(String nameUni);
 
     List<Degree> findByUniversity(University university);
+
+    Page<Degree> findByNameContainingOrFacultyContainingOrUniversityNameContaining(String name, String faculty, String uniName, Pageable pageable);
 }
